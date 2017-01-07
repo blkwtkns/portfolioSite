ea34f15f1500d8184f272985ad74a6f3a10d5517
Blake Watkins <iamsoblake@gmail.com>
2017-01-06 19:52:39 -0800

    added links to projects

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -20,7 +20,7 @@ const ProjectItem = props => {
 
     return (
         <div className="projectItem">
-          <h3>{props.projectItemData.position}, <span>{props.projectItemData.name}</span></h3>
+          <h3>{props.projectItemData.position}, <a href={props.projectItemData.website}><span>{props.projectItemData.name}</span></a></h3>
           <p className="projectDates">{getProjectDates()}</p>
           <ul>{projectSummaries}</ul>
         </div>
