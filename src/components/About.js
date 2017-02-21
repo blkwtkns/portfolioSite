57917ff729c6901e4265e0955b807b6b456ac18f
Blake Watkins <iamsoblake@gmail.com>
2017-02-21 13:21:15 -0800

    spy resume update

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const About = props => {
-    return (
-      <section className="about card">
-        <h2 className="text-uppercase"><i className="fa fa-lg fa-user"></i> About</h2>
-        <div>{props.aboutData}</div>
-      </section>
-    );
+  return (
+    <section className="about card">
+      <h2 className="text-uppercase"><i className="fa fa-lg fa-user"></i> About</h2>
+      <div>
+        {
+          props.aboutData.map((item, i) => {
+            return <p key={i}>{ item }</p>;
+          })
+        }
+      </div>
+    </section>
+  );
 };
 
 export default About;
